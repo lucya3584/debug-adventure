82d703a8918c0a0331b64f6c5e7096089fa70a4a
lucya3584 <lucya3584@yahoo.co.uk>
2019-11-01 20:36:46 +0000

    describe attack, added a return, not passing yet

--- src/enemy.js
+++ src/enemy.js
@@ -16,6 +16,7 @@ Object.defineProperty(Enemy.prototype, 'constructor', {
 
 Enemy.prototype._describeAttack = function (target) {
   `${this.name} lets out a ${this.dialogue}, and hits ${target.name} for ${this.damage} damage!`;
+  return this._describeAttack(target);
 };
 
 Enemy.prototype.attack = function (target) {
