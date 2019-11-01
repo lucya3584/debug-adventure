d3be6fdbfb77dcedeff6b69e118d87dac70fe25b
lucya3584 <lucya3584@yahoo.co.uk>
2019-11-01 19:12:28 +0000

    nine out of 12 ticks green, not complete yet

--- src/character.js
+++ src/character.js
@@ -1,5 +1,5 @@
 /* eslint-disable func-names */
-function Character() {
+function Character(config) {
   this.name = config.name;
   this.health = config.health;
   this.maxHealth = config.maxHealth;
@@ -10,22 +10,19 @@ Character.prototype = {
   get isAlive() {
     return this.health > 0;
   },
-};
-
-Character.prototype = {
   _takeDamage: function (damage) {
     this.health = damage;
     if (this.health < 0) {
-      this.health = 0;
-    }
-  }
+      this.health = 0; 
+    }   
+  },
   _heal: function (health) {
     this.health += health;
-    if (this.health < this.maxHealth) {
+    if (this.health > this.maxHealth) {
       this.health = this.maxHealth;
     }
   },
-  speak function () {
+  speak: function () {
     return this.dialog;
   },
 };
