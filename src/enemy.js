832bd798607f3e5515f906cf0591194c75f7e40c
lucya3584 <lucya3584@yahoo.co.uk>
2019-11-01 19:29:42 +0000

    constructor funtion passes, no passes in attack

--- src/enemy.js
+++ src/enemy.js
@@ -3,7 +3,7 @@ const Character = require('./character');
 
 function Enemy(config) {
   Character.call(this, config);
-  damage = config.damage;
+  this.damage = config.damage;
 }
 
 Enemy.prototype = Object.create(Character.prototype);
@@ -16,9 +16,9 @@ Object.defineProperty(Enemy.prototype, 'constructor', {
 
 Enemy.prototype._describeAttack = function (target) {
   `${this.name} lets out a ${this.dialogue}, and hits ${target.name} for ${this.damage} damage!`;
+};
 
-
-enemy.prototype.attack = function (target) {
+Enemy.prototype.attack = function (target) {
   target._takeDamage(damage);
   return this._describeAttack(target);
 };
